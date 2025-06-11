d08729545b061ed35fb7600fd6b5c35d8f549877
정민 문 <mjmkolpdd2@gmail.com>
2025-06-12 01:31:55 +0900

    qtype 추가 atomic, mutex의 자료형

--- OS_Queue222/qtype.h
+++ OS_Queue222/qtype.h
@@ -1,7 +1,8 @@
 #ifndef _QTYPE_H  // header guard
 #define _QTYPE_H
 
-// ==========이 파일은 수정 가능==========
+#include<atomic>
+#include<mutex>
 
 typedef unsigned int Key;  // 값이 클수록 높은 우선순위
 typedef void* Value;
@@ -20,7 +21,6 @@ typedef struct {
 typedef struct node_t {
     Item item;
     struct node_t* next;
-    // 필드 추가 가능
 } Node;
 
 typedef struct {
