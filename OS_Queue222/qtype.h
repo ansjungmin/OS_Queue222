fab2e1db746d54e08f9f56cb00c039ff4e1b7380
정민 문 <mjmkolpdd2@gmail.com>
2025-06-12 01:35:21 +0900

    qtype 노드 구조체 추가

--- OS_Queue222/qtype.h
+++ OS_Queue222/qtype.h
@@ -21,11 +21,13 @@ typedef struct {
 typedef struct node_t {
     Item item;
     struct node_t* next;
+    std::atomic<bool>marked;
 } Node;
 
 typedef struct {
     Node* head, tail;
-    // 필드 추가 가능
+    std::mutex queue_mutex;
+    std::atomic<int>size;
 } Queue;
 
 // 이후 자유롭게 추가/수정: 새로운 자료형 정의 등
